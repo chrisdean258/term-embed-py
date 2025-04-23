12870c754b2467047805409b99da834bfa77f1e4
chridean <>
2025-04-23 18:08:31 -0400

    sync

--- term-embed.py
+++ term-embed.py
@@ -5,13 +5,11 @@ import sys
 import os
 import select
 import tty
-<<<<<<< Updated upstream
-import fcntl
 import termios
 import struct
-=======
-import termios
->>>>>>> Stashed changes
+import fcntl
+
+log = open("log", "w")
 
 
 class Terminal:
@@ -25,6 +23,9 @@ class Terminal:
         self._cursor_c = 0
         self.child_pid = None
         self.pty = None
+        self.screen = [b' '] * (nrows * ncols)
+        self.normal_screen_contents = None
+        self.normal_screen_cursor = None
 
     @property
     def cursor(self):
@@ -33,6 +34,7 @@ class Terminal:
     @cursor.setter
     def cursor(self, cursor):
         self._cursor_r, self._cursor_c = cursor
+        self.move_cursor()
 
     @property
     def term_cursor(self):
@@ -42,13 +44,8 @@ class Terminal:
     def parse_cs(seq):
         if not seq.startswith(b"\033["):
             b']'
-<<<<<<< Updated upstream
-            return None, [], b''
-        p = 2
-=======
             return None, [], b'', False
         saw_question = False
->>>>>>> Stashed changes
         nums = []
         if seq[2:3] == b'?':
             saw_question = True
@@ -56,114 +53,145 @@ class Terminal:
         p = 2 + saw_question
         while p < len(seq):
             v = 0
-            while seq and b'0' <= seq[p:p+1] <= b'9':
+            digit = False
+            while p < len(seq) and b'0' <= seq[p:p+1] <= b'9':
+                digit = True
                 v *= 10
                 v += seq[p] - ord(b'0')
                 p += 1
-            nums.append(v)
+            if digit:
+                nums.append(v)
             if p >= len(seq):
                 break
             if seq[p:p+1] == b';':
                 p += 1
                 continue
             if p < len(seq):
-<<<<<<< Updated upstream
-                return seq[p:p+1], nums, seq[p + 1:]
-            break
-        return None, [], b''
-=======
                 return seq[p:p+1], nums, seq[p + 1:], saw_question
             break
         return None, [], b'', False
->>>>>>> Stashed changes
+
+    def passthrough(self, spec, args, private):
+        args = b';'.join(str(a).encode() for a in args)
+        val = b'\033[' + (b'?' if private else b'') + args + spec
+        self.tty.write(val)
 
     def handle_cs(self, spec, args, private):
-        if spec == b'n' and args and args[0] == 6:
-            a = b"\033[%d;%dR" % self.cursor
-<<<<<<< Updated upstream
-            b']'
-            os.write(self.pty, a)
-        elif spec == b'H' and len(args) == 2:
-            r, c = args
-            val = b'\x1b[%d;%dH' % (r + self.row, c + self.col)
-            b']'
-            self.tty.write(val)
+        a = b';'.join(str(a).encode() for a in args)
+        val = b'\033[' + (b'?' if private else b'') + a + spec
+        print("logging:", val, file=log, flush=True)
+        if spec in b'A':  # Move Cursor up
+            self._cursor_r = max(self._cursor_c - (args + [1])[0], 0)
+            self.move_cursor()
+        elif spec == b'B' and len(args) == 2:  # Move cursor Down
+            self._cursor_c = min(self._cursor_c + (args + [1])[0], self.nrows - 1)  # noqa:E501
+            self.move_cursor()
+        if spec == b'C':
+            self._cursor_c = min(self._cursor_c + (args + [1])[0], self.ncols)
+            self.move_cursor()
+        elif spec == b'D' and len(args) == 2:  # Move cursor left
+            self._cursor_c = max(self._cursor_c - (args + [1])[0], 0)
+            self.move_cursor()
+        elif spec == b'H':
+            self.cursor = (args + [0, 0])[:2]
+        elif spec == b'h' and args[0:1] == [1049]:
+            self.normal_screen_contents = self.screen
+            self.normal_screen_cursor = self.cursor
+            self.screen = [b' '] * (self.nrows * self.nrows)
+            self.cursor = (0, 0)
+            self.redraw()
+        elif spec == b'h':
+            self.passthrough(spec, args, private)
+        elif spec == b'J':
+            # if args and args[0] == 2: # this is right and need update
+            self.cursor = (0, 0)
+            self.screen = [b' '] * (self.ncols * self.nrows)
+            self.redraw()
+        elif spec == b'K':
+            if args and args[0] == 1:  # erase to left
+                start = self._cursor_r * self.ncols
+                end = self._cursor_r * self.ncols + self._cursor_c
+                cursor = self.cursor
+            elif args and args[0] == 2:  # erasse full line
+                start = self._cursor_r * self.ncols
+                end = start + self.ncols
+                cursor = (self._cursor_r, 0)
+            else:  # Erase to right
+                start = self._cursor_r * self.ncols + self._cursor_c
+                end = (self._cursor_r + 1) * self.ncols
+                cursor = self.cursor
+            self.screen[start:end] = [b' '] * (end - start)
+            self.tty.write(b' ' * (end - start))
+            self.cursor = cursor
             self.tty.flush()
-        else:
-            args = b';'.join(str(a).encode() for a in args)
-            val = b'\033[' + args + spec
-            b']'
-            self.tty.write(val)
-            self.tty.flush()
-=======
-            os.write(self.pty, a)
-        elif spec == b'h' and args[0:1] == [1049] and private:
-            # enter alternate buffer
-            pass
         elif spec == b'l' and args[0:1] == [1049] and private:
-            # leav alternate buffer
-            pass
-
-        elif spec == b'H' and len(args) == 2:
-            self.cursor = args
-            self.move_cursor()
-        elif spec == b'C' and len(args) == 2:
-            self._cursor_c = min(self._cursor_c + args[0], self.ncols)
-            self.move_cursor()
-        elif spec == b'D' and len(args) == 2:
-            self._cursor_c = max(self._cursor_c - args[0], 0)
-            self.move_cursor()
-        elif spec == b'm':
-            args = b';'.join(str(a).encode() for a in args)
-            val = b'\033[' + args + spec
-            self.tty.write(val)
+            self.screen = self.normal_screen_contents
+            self.normal_screen_contents = None
+            self.cursor = self.normal_screen_cursor
+            self.redraw()
+        elif spec == b'l':
+            self.passthrough(spec, args, private)
+        elif spec == b'm':  # Color
+            self.passthrough(spec, args, private)
+        elif spec == b'n' and args and args[0] == 6:
+            a = b"\033[%d;%dR" % (self._cursor_r + 1, self._cursor_c + 1)
+            os.write(self.pty, a)
+        elif spec == b'>' or spec == b'=':  # no exactly sure here. numpad?
+            self.passthrough(spec, args, private)
         else:
+            self.passthrough(spec, args, private)
             args = b';'.join(str(a).encode() for a in args)
-            val = b'\033[' + args + spec
-            print("skipping", val)
-            self.tty.write(val)
-            # self.tty.flush()
-            # os.write(self.pty, val)
->>>>>>> Stashed changes
+            val = b'\033[' + (b'?' if private else b'') + args + spec
+            print("skipping", val, file=log, flush=True)
+
+    def redraw(self):
+        HIDE_CURSOR = b'\x1b[?25l'
+        SHOW_CURSOR = b'\x1b[?12l\x1b[?25h'
+        cursor = self.cursor
+        ncols = self.ncols
+        self.tty.write(HIDE_CURSOR)
+        for i in range(len(self.screen) // ncols):
+            self.cursor = (i, 0)
+            content = b''.join(self.screen[ncols * i:ncols * (i + 1)])
+            self.tty.write(content)
+        self.cursor = cursor
+        self.tty.write(SHOW_CURSOR)
+        self.tty.flush()
+
+    def scroll_screen(self):
+        self.cursor = (self.nrows - 1, 0)
+        self.screen = self.screen[self.ncols:] + [b' '] * self.ncols
+        self.redraw()
+
+    def write_out(self, char):
+        max_size = self.nrows * self.ncols
+        idx = self._cursor_r * self.ncols + self._cursor_c
+        if idx >= max_size:
+            self.scroll_screen()
+            idx -= self.ncols
+        self.screen[idx] = char
+        self.tty.write(char)
+        self._cursor_c += 1
+        if self._cursor_c >= self.ncols:
+            self.inc_row()
+            self._cursor_c = 0
 
     def inc_row(self):
         if self._cursor_r < self.nrows:
             self._cursor_r += 1
+        else:
+            self.scroll_screen()
 
     def move_cursor(self):
         MOVE_CURSOR = b'\x1b[%d;%dH'
         self.tty.write(MOVE_CURSOR % (self._cursor_r + self.row, self._cursor_c + self.col))  # noqa:E501
 
     def start(self):
-<<<<<<< Updated upstream
-        self.pty, child = pty.openpty()
-        print("hi")
-        self.child_pid = os.fork()
-        if self.child_pid == 0:
-            os.dup2(child, 0)
-            os.dup2(child, 1)
-            os.dup2(child, 2)
-            winsize = struct.pack("HH", self.nrows, self.ncols)
-            fcntl.ioctl(self.pty, termios.TIOCSWINSZ, winsize)
-            os.close(self.pty)
-            os.close(child)
-            os.execlp("/bin/bash", "/bin/bash", "-i")
-            sys.exit(1)
-        ipt = open(sys.stdin.fileno(), "rb")
-        tty.setraw(ipt.fileno())
-        os.set_blocking(ipt.fileno(), False)
-        try:
-            return self._start(ipt)
-        finally:
-            os.close(self.pty)
-
-    def _start(self, ipt):
-        while check_pid(self.child_pid):
-            read, _, _ = select.select([self.pty, ipt], [], [])
-=======
         self.child_pid, self.pty = pty.fork()
-        self._start()
-        os.wait()
+        try:
+            self._start()
+        except OSError:
+            pass
 
     def _start(self):
         self.move_cursor()
@@ -175,30 +203,18 @@ class Terminal:
             os.execlp("/bin/sh", "/bin/sh", "-i")
             sys.exit(1)
 
-        stdin = open(sys.stdin.fileno(), "rb")
+        stdin = sys.stdin.buffer
         tty.setraw(stdin.fileno())
         stdin.flush()
-        os.set_blocking(stdin.fileno(), False)
 
-        while True:
+        while os.waitpid(self.child_pid, os.WNOHANG) == (0, 0):
             read, _, _ = select.select([self.pty, stdin], [], [])
->>>>>>> Stashed changes
             for row in read:
                 if row == self.pty:
                     blocks = []
-                    while select.select([self.pty], [], [], 0)[0]:
-                        try:
-                            blocks.append(os.read(self.pty, 1024))
-<<<<<<< Updated upstream
-                        except IOError:
-                            break
-
+                    while select.select([self.pty], [], [], 0)[0] and (c := os.read(self.pty, 1024)):  # noqa:E501
+                        blocks.append(c)
                     chs = b''.join(blocks)
-=======
-                        except OSError:
-                            return
-                    chs = b'!!'.join(blocks)
->>>>>>> Stashed changes
                     i = 0
                     while i < len(chs):
                         c = chs[i:]
@@ -207,63 +223,36 @@ class Terminal:
                             control, args, rest, private = Terminal.parse_cs(c)
                             if control:
                                 chs, i = rest, 0
-<<<<<<< Updated upstream
-                                self.handle_cs(control, args)
-=======
                                 self.handle_cs(control, args, private)
->>>>>>> Stashed changes
                                 continue
                         elif c.startswith(b"\n"):
                             self._cursor_c = 0
                             self.inc_row()
-<<<<<<< Updated upstream
-                            # self.tty.write(b'\x1b[%d;%dH' % self.term_cursor)
-                            continue
-
-                        elif c.startswith(b"\r"):
-                            self._cursor_c = 0
-                            # self.tty.write(b'\x1b[%d;%dH' % self.term_cursor)
-=======
                             self.move_cursor()
                             continue
                         elif c.startswith(b"\r"):
                             self._cursor_c = 0
                             self.move_cursor()
->>>>>>> Stashed changes
+                            continue
+                        elif c.startswith(b"\b"):
+                            self._cursor_c = max(self._cursor_c - 1, 0)
+                            self.move_cursor()
+                            self.tty.flush()
                             continue
                         else:
-                            self._cursor_c += 1
-                            if self._cursor_c >= self.ncols:
-                                self.inc_row()
-                                self._cursor_c = 0
-<<<<<<< Updated upstream
-                        self.tty.write(c[:1])
-                        self.tty.flush()
-                        i += 1
-                if row == ipt:
-                    c = ipt.read(1024)
-                    os.write(self.pty, c)
-        os.wait()
-=======
-                            self.tty.write(c[:1])
+                            try:
+                                if not c[:1].decode().isprintable():
+                                    print("Not handling:", c[:5], file=log)
+                            except Exception:
+                                print("Not handling:", c[:5], file=log)
+                            print("logging:", c[:1], file=log, flush=True)
+                            self.write_out(c[:1])
                         self.tty.flush()
                 if row == stdin:
-                    try:
-                        c = stdin.read(1024)
-                        os.write(self.pty, c)
-                    except OSError:
-                        return
->>>>>>> Stashed changes
-
-
-def check_pid(pid):
-    """ Check For the existence of a unix pid. """
-    try:
-        os.kill(pid, 0)
-    except OSError:
-        return False
-    else:
-        return True
+                    os.set_blocking(stdin.fileno(), False)
+                    c = stdin.read(1024)
+                    os.set_blocking(stdin.fileno(), True)
+                    os.write(self.pty, c)
 
 
 def get_terminal_size(fd):
@@ -271,7 +260,7 @@ def get_terminal_size(fd):
         rows, columns = os.get_terminal_size()
         return rows, columns
     except OSError:
-        return 80, 25
+        pass
 
     try:
         s = struct.pack("HHHH", 0, 0, 0, 0)
@@ -283,16 +272,14 @@ def get_terminal_size(fd):
 
 
 def main():
-<<<<<<< Updated upstream
-    rows, cols = get_terminal_size(sys.stdout.fileno())
-    print(rows, cols)
-    t = Terminal(open(sys.stdout.fileno(), "wb"), 10, 10, rows - 20, cols - 20)
-    t.start()
-=======
     sys.stdout.write('\x1b[?1049h')
     sys.stdout.write('\033[2J')
     sys.stdout.flush()
-    t = Terminal(open(sys.stdout.fileno(), "wb"), 10, 10, 25, 100)
+    print('\x1b[;H' + "+\n" * 8 + 'x' * 8 + "+" + '-' * 100 + "+")
+    for i in range(25):
+        print(8 * 'x' + "|" + ' ' * 100 + "|")
+    print(8*'x' + "+" + '-' * 100 + "+", flush=True)
+    t = Terminal(sys.stdout.buffer, 10, 10, 25, 100)
     old_settings = termios.tcgetattr(0)
     tty.setraw(sys.stdin.fileno())
     try:
@@ -303,7 +290,6 @@ def main():
         tty.setcbreak(sys.stdin.fileno())
         termios.tcsetattr(0, termios.TCSADRAIN, old_settings)
 
->>>>>>> Stashed changes
     return 0
 
 
